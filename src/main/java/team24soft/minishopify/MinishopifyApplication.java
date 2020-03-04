e1b4a2c593ed33d2440358234f2382c5bdde2ea7
MrJimenez <j0s3.fr4nc0@gmail.com>
2020-03-04 16:30:07 -0500

    POM configuration (we might need to remove some dependencies)

---
+++ src/main/java/team24soft/minishopify/MinishopifyApplication.java
@@ -0,0 +1,14 @@
+package team24soft.minishopify;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MinishopifyApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(MinishopifyApplication.class, args);
+    }
+}
+
+
+
