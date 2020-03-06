2ef8c6cdb52e4f362d814722a44a1ee7bd7c20db
Jose Franco <32456973+JoseFrancoJimenez@users.noreply.github.com>
2020-03-05 19:14:53 -0500

    Revert "Created constructor for the Product class (#26)" (#29)
    This reverts commit 6f68192b5ae423dc894eeaac2c5d045f4a1bbabd.

--- src/main/java/team24soft/minishopify/models/Product.java
+++ src/main/java/team24soft/minishopify/models/Product.java
@@ -1,32 +1,15 @@
 package team24soft.minishopify.models;
 
+
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
-import javax.swing.*;
-import java.util.Objects;
 
 @Entity
 public class Product {
-    private String title; //name given to product
-    private String description; //description of the product
-    private int price; //price of the product
-    private ImageIcon image;
-
     @Id
     @GeneratedValue(strategy= GenerationType.AUTO)
-    private Long inventoryNumber;
-
-    public Product(){
-
-    }
-
-    public Product(String title, String description, int price){
-        this.title = title;
-        this.description = description;
-        this.price = price;
-        image = new ImageIcon(getClass().getResource("/images/default.jpg"));
-    }
+    public long id;
 
 }
