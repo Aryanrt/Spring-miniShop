5776e73957b08b7731725deeb25dff77286ed2cc
almasrur <44327442+almasrur@users.noreply.github.com>
2020-03-05 20:23:39 -0500

    Created constructor for Product class (#31)

--- src/main/java/team24soft/minishopify/models/Product.java
+++ src/main/java/team24soft/minishopify/models/Product.java
@@ -1,15 +1,31 @@
-//package team24soft.minishopify.models;
-//
-//
-//import javax.persistence.Entity;
-//import javax.persistence.GeneratedValue;
-//import javax.persistence.GenerationType;
-//import javax.persistence.Id;
-//
-//@Entity
-//public class Product {
-//    @Id
-//    @GeneratedValue(strategy= GenerationType.AUTO)
-//    public long id;
-//
-//}
+package team24soft.minishopify.models;
+
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class Product {
+    private String title; //name given to product
+    private String description; //description of the product
+    private int price; //price of the product
+    private String image;
+
+    @Id
+    @GeneratedValue(strategy= GenerationType.AUTO)
+    private Long inventoryNumber;
+
+    public Product(){
+
+    }
+
+    public Product(String title, String description, int price, String image) {
+        this.title = title;
+        this.description = description;
+        this.price = price;
+        this.image = image;
+    }
+
+}
